d97936cc30a783d1a85f09d2f87e90d393f3d4c8
Shrividya Gopi <shrividya.gopi@gmail.com>
2024-10-22 20:51:28 -0400

    completed Loop exercise

---
+++ Loopsexample/Program.cs
@@ -0,0 +1,83 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+       /* Console.WriteLine("Print 15 times of while loop :");
+        for (int i = 0; i < 15; i++)
+        {
+            Console.WriteLine(i);
+        }*/
+        Console.WriteLine("Press 2nd time to print 1-5 times:...");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 3rd time to print 1-5 times:...");
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 4th time to print 5 times:...");
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 5th time to print 1-5 times:...");
+        for (int i = 0; i < 5; i++) 
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 6th time to print 1-5 times:...  ");
+        for (int i = 0; i <= 5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 7th time to print 1-5 times:...  ");
+        for (int i = 0; i < 2; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 8th time to print 1-5 times:...  ");
+        for (int i = 0; i <=4; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 9th time to print 1-5 times:...  ");
+        for (int i = 0; i <4; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 10th time to print 1-5 times:...  ");
+        for (int i = 0; i <=5; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 11th time to print 1-5 times:...  ");
+        for (int i = 0; i <=8; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 12th time to print 1-5 times:...  ");
+        for (int i = 0; i <=3; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 13th time to print 1-5 times:...  ");
+        for (int i = 0; i <=6; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 14th time to print 1-5 times:...  ");
+        for (int i = 0; i <=2; i++)
+        {
+            Console.WriteLine(i);
+        }
+        Console.WriteLine("Press 15th time to print 1-5 times:...  ");
+        for (int i = 0; i <=3; i++)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
